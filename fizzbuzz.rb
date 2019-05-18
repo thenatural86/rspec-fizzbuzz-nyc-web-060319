519e9935cfeff8eb367df2b26c19f736d7bba477
The Natural <philip.smalls86@gmail.com>
2019-05-18 02:23:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,12 +5,13 @@ def fizzbuzz(int)
     "Fizz"
   elsif int % 5 == 0 
     "Buzz"
+  else int % 3 == 0 && int % 5 == 0 
+    "FizzBuzz"
     
   end
 end
 
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0 
-    "FizzBuzz"
+
+   
   end
 end
